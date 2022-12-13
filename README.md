ff7d4c6a3577a015b49f61157913885b10880c3b
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-13 18:17:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Open website
+ &Open website 
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  // &Open
+
+  return (
+    <div className="App">
+       <Navbar />
+    </div>
+  )
+}
+
+export default App
