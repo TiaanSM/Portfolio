672e69caf8246e70bda960b7066058a2b324f9c5
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-12-14 15:54:02 +0200

    Completed header component.
    Header layout finished and responsive.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,17 @@ const Hero = () => {
     <header>
         <div className="hero-img-container">
             <img src={img1} className="hero-image img1" alt="" />
-            <img src={img2} className="hero-image img2" alt="" />
-            <img src={img3} className="hero-image img3" alt="" />
+            <img src={img5} className="hero-image img2" alt="" />
+            <img src={img2} className="hero-image img3" alt="" />
             <img src={img4} className="hero-image img4" alt="" />
-            <img src={img5} className="hero-image img5" alt="" />
+            <img src={img3} className="hero-image img5" alt="" />
+        </div>
+        <div className="hero-text-container">
+          <h1 className="hero-title">Corporate gifting <br/><span className="text-tail">made easy</span></h1>
+          <div className="hero-cta-container">
+            <p className="hero-text-desc">Discover better gifting, at any scale,<br/> for companies that care.</p>
+            <button className="hero-cta">GET STARTED</button>
+          </div>
         </div>
     </header>
   )
